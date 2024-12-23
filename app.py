3207466b08ada46dc2af40ade1fb8342e8e71a0d
arjya-cmd <arjyamazumdar1815@gmail.com>
2024-12-23 14:03:31 +0530

    Updated Flask app for cloud deployment

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+       port = int(os.environ.get("PORT", 5000))          app.run(host="0.0.0.0", port=port, debug=True)
